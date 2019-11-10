c6336050ba15a9924f3b7842a331c3988834d110
Bailey Pollard <45129610+Baileypollard@users.noreply.github.com>
2019-11-09 22:42:55 -0400

    Added Lava maze scouting

--- tasks/LavaMazeGatheringInformationTask.java
+++ tasks/LavaMazeGatheringInformationTask.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-
+import org.osbot.rs07.api.Equipment;
 import org.osbot.rs07.api.def.ItemDefinition;
 import org.osbot.rs07.api.map.Position;
 import org.osbot.rs07.api.model.Player;
@@ -7,34 +6,35 @@ import org.osbot.rs07.event.WebWalkEvent;
 import org.osbot.rs07.script.MethodProvider;
 import org.osbot.rs07.utility.Condition;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class RevGatheringInformationTask extends Task {
+public class LavaMazeGatheringInformationTask extends Task {
     private List<Player> localPlayers;
 
-    Position southRevs = new Position(3250, 10141, 0);
-    Position northRevs = new Position(3244, 10167, 0);
+    Position northMaze = new Position(3024, 3839, 0);
+    Position southMaze = new Position(3034, 3822, 0);
 
-    public RevGatheringInformationTask(MethodProvider api, String name) {
+    public LavaMazeGatheringInformationTask(MethodProvider api, String name) {
         super(api, name);
         localPlayers = new ArrayList<>();
     }
 
     @Override
     public boolean canProcess() {
-        return Loc.REV_SCOUT_AREA.getArea().contains(api.myPosition());
+        return Loc.LAVA_MAZE_SCOUT_AREA.getArea().contains(api.myPosition());
     }
+
     //
     @Override
     public void process() throws InterruptedException {
         DiscordWebhook webhook = new DiscordWebhook("https://discordapp.com/api/webhooks/631977624641470464/otnghB7jkjvFoNRT89y9TzxJa6DwPJq1WZPesIEL_b_EwLMcHIUdZQ3tghVx2H7XWia9");
-        api.log("NEW");
 
-        final WebWalkEvent south = new WebWalkEvent(southRevs);
+        final WebWalkEvent south = new WebWalkEvent(northMaze);
         south.setBreakCondition(new Condition() {
             @Override
             public boolean evaluate() {
@@ -45,7 +45,7 @@ public class RevGatheringInformationTask extends Task {
         api.execute(south);
 
         if (south.hasFinished()) {
-            final WebWalkEvent north = new WebWalkEvent(northRevs);
+            final WebWalkEvent north = new WebWalkEvent(southMaze);
             south.setBreakCondition(new Condition() {
                 @Override
                 public boolean evaluate() {
@@ -65,12 +65,9 @@ public class RevGatheringInformationTask extends Task {
         }
 
         List<Player> allPlayers = api.getPlayers().getAll();
-        for (Player player : allPlayers)
-        {
-            if (isNewPlayer(player) && !api.myPlayer().getName().equals(player.getName()))
-            {
-                if (player.getCombatLevel() > 35 && player.getCombatLevel() < 90)
-                {
+        for (Player player : allPlayers) {
+            if (isNewPlayer(player) && !api.myPlayer().getName().equals(player.getName())) {
+                if (player.getCombatLevel() > 35 && player.getCombatLevel() < 90) {
                     LocalDateTime myDateObj = LocalDateTime.now();
                     System.out.println("Before formatting: " + myDateObj);
                     DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MMM dd HH:mm:ss");
@@ -79,22 +76,19 @@ public class RevGatheringInformationTask extends Task {
                     String skulled = player.getSkullIcon() == 0 ? "**SKULLED**" : "";
                     String world = "[" + formattedDate + "] W" + api.getWorlds().getCurrentWorld();
                     String message = world + " " + skulled + " " + player.getName() + " (" +
-                            player.getCombatLevel() + ") " + getOthersEquipment(player).toString();
+                            player.getCombatLevel() + ") " + getOthersEquipment(player).toString() + " RISKING " + calculateRisk(player);
 
                     api.log(message);
                     localPlayers.add(player);
                     webhook.setContent(message);
-                    try
-                    {
+                    try {
                         webhook.execute();
-                    } catch (IOException e)
-                    {
+                    } catch (IOException e) {
                         api.log(e.getLocalizedMessage());
                     }
                 }
             }
         }
-
 
 
     }
@@ -107,13 +101,56 @@ public class RevGatheringInformationTask extends Task {
                 if (equipment[i] - 512 > 0) {
                     String name = ItemDefinition.forId(equipment[i] - 512).getName();
                     equipmentList.add(name);
-                    ItemResource item = new ItemResource(name, equipment[i] - 512);
                 }
+
             }
         }
         return equipmentList;
     }
+//ghostface147@hotmail.com
+//pass
+//4225124jr
 
+    private String calculateRisk(Player p) {
+        int first = 0;
+        int second = 0;
+        int third = 0;
+
+        int total = 0;
+
+        List<String> equipmentList = new LinkedList<String>();
+
+        if (p != null) {
+            int[] equipment = p.getDefinition().getAppearance();
+            for (int i = 0; i < equipment.length; i++) {
+                if (equipment[i] - 512 > 0) {
+                    String name = ItemDefinition.forId(equipment[i] - 512).getName();
+                    equipmentList.add(name);
+                    ItemResource item = new ItemResource(name, equipment[i] - 512);
+                    if (item.getPrice() != -1) {
+                        total += item.getPrice();
+                    }
+
+                    if (item.getPrice() > first) {
+                        third = second;
+                        second = first;
+                        first = item.getPrice();
+                    } else if (item.getPrice() > second) {
+                        third = second;
+                        second = item.getPrice();
+                    } else if (item.getPrice() > third)
+                        third = item.getPrice();
+
+                }
+            }
+            if (p.getSkullIcon() == 0) {
+                return Integer.toString(total);
+            } else {
+                return Integer.toString(total - (first - second - third));
+            }
+        }
+        return "N/A";
+    }
 
     public boolean isNewPlayer(Player player) {
         for (Player p : localPlayers) {
